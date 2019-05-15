f6bb4d6db75cfb46150618de0d2cc8e275436baf
divvany <andreeascurtu@rocketmail.com>
2019-05-15 22:33:46 +0300

    Update info_license.php

--- info_license.php
+++ info_license.php
@@ -1,4 +1,4 @@
 <?php
 //license file will be placed in /var/www/.config
-$output = shell_exec('refines NS3.csp< input_info_fdr > out');
+$output = shell_exec('refines NS3.csp < input_info_fdr > out');
 ?>
